f477b2a2b2f48086e60c6f44afe35014d36cd092
GenkiShimizu <mizuyoukan000@gmail.com>
2024-06-22 16:34:06 +0900

    88完成

--- Practice/88.py
+++ Practice/88.py
@@ -2,7 +2,21 @@ N = int(input())
 
 M = []
 
-for _ in range(N):
-    row = list(str, input())
+for _ in range(0, N):
+    si = input()
+    si = list(si)
+    M.append(si)
 
-    M.append(row)
+for i in range(N - 2, -1, -1):
+    for j in range(1, 2*N-2):
+        if M[i][j] == '#':
+            if M[i][j-1] == 'X':
+                M[i][j] = 'X'
+            elif M[i][j] == 'X':
+                M[i][j] = 'X'
+            elif M[i][j+1] == 'X':
+                M[i][j] = 'X'
+
+for i in range(0, N):
+    M[i] = ''.join(M[i])
+    print(M[i])
